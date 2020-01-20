4b7c11b049c18f03c5ff7f9bda5ee7dbdd95dceb
jedel <jedeland@live.co.uk>
2020-01-20 20:58:51 +0100

    Updated file to show the pixelated number

--- venv/recog_digits.py
+++ venv/recog_digits.py
@@ -1,6 +1,10 @@
 #To init, pip install scikit-learn
 from sklearn import svm, datasets
+import matplotlib.pyplot as plt
 
 svc_set = svm.SVC(gamma=0.001, C=100.) #Support Vector Class
 digits = datasets.load_digits()
 print(digits.DESCR) #Prints out textual description of the dataset
+print(digits.images[0])
+plt.imshow(digits.images[0], cmap=plt.cm.gray_r, interpolation="nearest")
+plt.show()
